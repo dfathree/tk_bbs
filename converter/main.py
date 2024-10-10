b7cc9fa3f4d0d4c85a94ac931a5d64023741bf4a
dfa <daf@example.com>
2024-10-10 09:50:14 +0900

    add encoding converter

---
+++ converter/main.py
@@ -0,0 +1,14 @@
+# dat ディレクトリ以下のファイルを読み込んで、ファイル名を表示する
+import glob
+import nkf
+
+files = glob.glob("dat/*/*/??????????")
+for file in files:
+    with open(file, "br") as f:
+        file_data = f.read()
+        # nkf で file の文字コードを判定する
+    output = nkf.nkf('-Sw', file_data)
+    # file + ".txt" というファイル名で output を保存する
+    with open(file + ".txt", "bw") as f:
+        f.write(output)
+
