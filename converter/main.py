367bf3a747031b68eae22a302af991bd4e36a3e6
dfa <daf@example.com>
2024-10-14 08:57:31 +0900

    replace <br> to linefeed

--- converter/main.py
+++ converter/main.py
@@ -7,7 +7,7 @@ for file in files:
     with open(file, "br") as f:
         file_data = f.read()
         # nkf で file の文字コードを判定する
-    output = nkf.nkf('-Sw', file_data)
+    output = nkf.nkf("-Sw", file_data).replace(b"<br>", b"\\n")
     # file + ".txt" というファイル名で output を保存する
     with open(file + ".txt", "bw") as f:
         f.write(output)
