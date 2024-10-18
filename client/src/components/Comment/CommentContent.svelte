59951cf7dd25df39d000173fbda5f65ed90cc2e2
Yosuke Makita <Yosuke.Makita@access-company.com>
2024-10-19 07:04:26 +0900

    コメント削除機能実装 (#6)

--- client/src/components/Comment/CommentContent.svelte
+++ client/src/components/Comment/CommentContent.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
-  import { Button } from 'flowbite-svelte'
   import { CaretRightSolid } from 'flowbite-svelte-icons'
   import type { Comment } from '../ResBox/types'
+  import DeleteCommentButton from './DeleteCommentButton.svelte'
   import { formatDate } from '$lib/DateFormatter'
 
+  export let threadId: string
+  export let resId: string
   export let comment: Comment
 
   const urlRegexp = /(h?ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+)/g
 
   $: lines = comment.content.split(urlRegexp).filter(Boolean)
-
-  const handleDelete = () => {
-    if (confirm('本当に削除しますか？')) {
-      alert('削除しました')
-      // fetch(`/api/comments/${comment.id}`, { method: 'DELETE' }).then(res => {
-      //   if (res.ok) {
-      //     location.reload()
-      //   } else {
-      //     alert('削除に失敗しました')
-      //   }
-      // })
-    }
-  }
 </script>
 
 <div>
@@ -41,5 +30,5 @@
     {/if}
   {/each}
   <span class="ms-2">{formatDate(comment.createdAt)}</span>
-  <Button class="text-xs ms-2 px-2 py-0.5" on:click={handleDelete}>削除</Button>
+  <DeleteCommentButton {threadId} {resId} commentId={comment.commentId} on:deleteComment />
 </div>
