ec1ea8a525a190a1a0ae99cda2635cc77fcfa6e5
aYosukeMakita <yosuke.makita@access-company.com>
2024-10-27 18:15:22 +0900

    親レス編集機能実装

---
+++ client/src/components/Parent/ParentPulldownMenu.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+  import { Button, Dropdown, DropdownItem } from 'flowbite-svelte'
+  import { BarsOutline } from 'flowbite-svelte-icons'
+  import EditParentButton from './EditParentButton.svelte'
+
+  export let threadId: string
+  let openDropdown = false
+</script>
+
+<Button class="p-1" size="xs" color="light" pill><BarsOutline /></Button>
+<Dropdown bind:open={openDropdown}>
+  <EditParentButton {threadId} on:editParent on:close={() => (openDropdown = false)}>編集</EditParentButton>
+  <DropdownItem>削除</DropdownItem>
+</Dropdown>
