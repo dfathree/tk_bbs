812b6564b96d58bf702d02c4f647e8694dfdcf25
aYosukeMakita <yosuke.makita@access-company.com>
2024-11-02 21:29:15 +0900

    サイドバーにフォーカスを当てる

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import { page as storePage } from '$app/stores'
   import { PUBLIC_API_SERVER } from '$env/static/public'
   import { boardStore, type BoardType } from '../store/boardStore'
+
+  let threadId = ''
 
   onMount(async () => {
     const response = await fetch(`${PUBLIC_API_SERVER}/api/board/get.php`)
     const { threads }: { threads: BoardType[] } = await response.json()
     boardStore.update(() => threads)
+  })
+  onMount(() => {
+    const unsubscribe = storePage.subscribe(async $page => {
+      threadId = $page.params.threadId
+    })
+    return () => {
+      unsubscribe()
+    }
   })
 </script>
 
@@ -14,9 +25,15 @@
   <h2>Boards</h2>
   <ul>
     {#each $boardStore as board}
-      <li>
+      <li class={board.threadId === threadId ? 'active' : undefined}>
         <a href={`/threads/${board.threadId}`}>{board.title}</a>
       </li>
     {/each}
   </ul>
 </div>
+
+<style scoped>
+  .active {
+    background-color: #d0d0d0;
+  }
+</style>
