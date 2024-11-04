d4437a23490921790036b694a28ce415540568e3
Yosuke Makita <Yosuke.Makita@access-company.com>
2024-11-04 17:45:43 +0900

    スレ立て機能 (#19)

--- client/src/store/boardStore.ts
+++ client/src/store/boardStore.ts
@@ -1,4 +1,5 @@
 import { writable } from 'svelte/store'
+import { PUBLIC_API_SERVER } from '$env/static/public'
 
 export type BoardType = {
   threadId: string
@@ -6,4 +7,17 @@ export type BoardType = {
   lastModified: string
 }
 
-export const boardStore = writable<BoardType[]>([])
+const { subscribe, set, update } = writable<BoardType[]>([])
+
+const fetchFromNetwork = async () => {
+  const response = await fetch(`${PUBLIC_API_SERVER}/api/board/get.php`)
+  const { threads }: { threads: BoardType[] } = await response.json()
+  update(() => threads)
+}
+
+export const boardStore = {
+  subscribe,
+  set,
+  update,
+  fetch: fetchFromNetwork,
+}
