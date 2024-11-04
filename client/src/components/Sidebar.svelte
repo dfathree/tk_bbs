5072e6467de1af6ae9ebbfded7ff4d430adf3753
aYosukeMakita <yosuke.makita@access-company.com>
2024-11-04 18:01:41 +0900

    サイドバー修正

--- client/src/components/Sidebar.svelte
+++ client/src/components/Sidebar.svelte
@@ -10,19 +10,26 @@
   $: threadId = $page.url.pathname.match(/^\/threads\/(\d+)/)?.[1]
 </script>
 
-<div>
-  <h2>Boards</h2>
+<div class="mt-1 ml-2 border-r-2">
+  <h2 class="text-xl">スレッド一覧</h2>
   <ul>
     {#each $boardStore as board}
       <li class={board.threadId === threadId ? 'active' : undefined}>
-        <a href={`/threads/${board.threadId}`}>{board.title}</a>
+        <a
+          class="hover:text-gray-900 text-gray-500 dark:text-gray-400 dark:hover:text-white"
+          href={`/threads/${board.threadId}`}>{board.title}</a
+        >
       </li>
     {/each}
   </ul>
 </div>
 
 <style scoped>
-  .active {
-    background-color: #d0d0d0;
+  li {
+    margin: 0.25rem 0;
+  }
+  .active a {
+    color: rgb(235 79 39);
+    font-weight: bold;
   }
 </style>
